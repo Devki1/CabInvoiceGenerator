ae24b052be3e2ff8b146041bceca8086a1af8678
Devki1 <gdev3195@gmail.com>
2020-04-07 12:49:38 +0530

    ADDED:Create a method to calculate fare

--- src/main/java/com/bridgelabz/service/CabInvoiceService.java
+++ src/main/java/com/bridgelabz/service/CabInvoiceService.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.service;
 
 public class CabInvoiceService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator");
+    private static final double RATE_PER_KM = 10;
+    private static final int RATE_PER_MIN = 1;
+    private static final double MINIMUM_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = RATE_PER_KM * distance + RATE_PER_MIN * time;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
+
