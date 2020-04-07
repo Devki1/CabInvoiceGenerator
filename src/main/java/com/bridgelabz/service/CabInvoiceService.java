c9f6451a9213bd2e749a11efe7007df423392ec6
Devki1 <gdev3195@gmail.com>
2020-04-07 11:39:27 +0530

    ADDED:Printing a welcome message

---
+++ src/main/java/com/bridgelabz/service/CabInvoiceService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.service;
+
+public class CabInvoiceService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator");
+    }
+}
