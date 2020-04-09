c6994e99d93ce1ba023eead52fe2e0eaf9662ec3
Devki1 <gdev3195@gmail.com>
2020-04-09 23:51:37 +0530

    ADDED:Test case invoice summary and fare of user

--- src/test/java/com/bridgelabz/servicetest/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/CabInvoiceServiceTest.java
@@ -1,27 +1,34 @@
 package com.bridgelabz.servicetest;
 
 import com.bridgelabz.service.CabInvoiceService;
+import com.bridgelabz.service.InvoiceSummary;
 import com.bridgelabz.service.Ride;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceServiceTest {
-    CabInvoiceService cabInvoiceService = new CabInvoiceService();
+    CabInvoiceService cabInvoiceGenerator = null;
+
+    @Before
+    public void setUp() {
+        cabInvoiceGenerator = new CabInvoiceService();
+    }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        double distance = 4.0;
+        double distance = 5.0;
         int time = 15;
-        double fare = cabInvoiceService.calculateFare(distance, time);
-        Assert.assertEquals(55, fare, 0);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(65, fare, 0.0);
     }
-    @Test
-    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
 
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
         double distance = 0.1;
-        int time = 3;
-        double fare = cabInvoiceService.calculateFare(distance, time);
-        Assert.assertEquals(5, fare, 0);
+        int time = 2;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 
     @Test
@@ -31,9 +38,22 @@ public class CabInvoiceServiceTest {
                 new Ride(0.1, 1),
                 new Ride(4.1, 25)
         };
-        double totalFare = cabInvoiceService.calculateFareForMultipleRides(rides);
+        double totalFare = cabInvoiceGenerator.calculateFareForMultipleRides(rides);
         Assert.assertEquals(96, totalFare, 0);
     }
+
+    @Test
+    public void givenUserIdAndRide_shouldReturnInvoiceSummary() {
+        String userId = "gdev3123@gmail.com";
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1),
+                new Ride(4.1, 25)
+        };
+        cabInvoiceGenerator.addRides(userId);
+        InvoiceSummary invoiceSummary = cabInvoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary fare = new InvoiceSummary(3, 96);
+        Assert.assertEquals(fare, invoiceSummary);
+        Assert.assertEquals(rides.length, cabInvoiceGenerator.getRidesByUserId(userId).size());
+    }
 }
-
-
