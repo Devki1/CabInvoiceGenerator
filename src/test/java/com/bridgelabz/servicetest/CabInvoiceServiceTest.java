162ab8719864013cc2475f2457e16c9b95d565ad
Devki1 <gdev3195@gmail.com>
2020-04-10 13:31:03 +0530

    ADDED:Test cases for premimum rides

--- src/test/java/com/bridgelabz/servicetest/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/CabInvoiceServiceTest.java
@@ -8,52 +8,90 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceServiceTest {
-    CabInvoiceService cabInvoiceGenerator = null;
-
-    @Before
-    public void setUp() {
-        cabInvoiceGenerator = new CabInvoiceService();
-    }
+    CabInvoiceService invoiceServiceForPremimum = new CabInvoiceService(CabInvoiceService.subscriptionPlan.PremimumRides);
+    CabInvoiceService invoiceServiceForNormal = new CabInvoiceService(CabInvoiceService.subscriptionPlan.NormalRides);
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+    public void givenDistanceAndTime_withNormalPlan_shouldReturnTotalFare() {
         double distance = 5.0;
         int time = 15;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceServiceForNormal.calculateFare(distance, time);
         Assert.assertEquals(65, fare, 0.0);
     }
 
     @Test
-    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+    public void givenLessDistanceAndTime_withNormalPlan_shouldReturnMinFare() {
         double distance = 0.1;
         int time = 2;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceServiceForNormal.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+    public void givenMultipleRides_withNormalPlan_shouldReturnInvoiceSummary() {
         Ride[] rides = {
                 new Ride(2.0, 5),
                 new Ride(0.1, 1),
                 new Ride(4.1, 25)
         };
-        double totalFare = cabInvoiceGenerator.calculateFareForMultipleRides(rides);
+        double totalFare = invoiceServiceForNormal.calculateFareForMultipleRides(rides);
         Assert.assertEquals(96, totalFare, 0);
     }
 
     @Test
-    public void givenUserIdAndRide_shouldReturnInvoiceSummary() {
+    public void givenUserIdAndRide_withNormalPlan_shouldReturnInvoiceSummary() {
         String userId = "gdev3123@gmail.com";
         Ride[] rides = {
                 new Ride(2.0, 5),
                 new Ride(0.1, 1),
                 new Ride(4.1, 25)
         };
-        cabInvoiceGenerator.addRides(userId);
-        InvoiceSummary invoiceSummary = cabInvoiceGenerator.getInvoiceSummary(rides);
+        invoiceServiceForNormal.addRides(userId);
+        InvoiceSummary invoiceSummary = invoiceServiceForNormal.getInvoiceSummary(rides);
         InvoiceSummary fare = new InvoiceSummary(3, 96);
         Assert.assertEquals(fare, invoiceSummary);
-        Assert.assertEquals(rides.length, cabInvoiceGenerator.getRidesByUserId(userId).size());
+        Assert.assertEquals(rides.length, invoiceServiceForNormal.getRidesByUserId(userId).size());
+    }
+
+    @Test
+    public void givenDistanceAndTime_withPremimumPlan_shouldReturnTotalFare() {
+        double distance = 5.0;
+        int time = 15;
+        double fare = invoiceServiceForPremimum.calculateFare(distance, time);
+        Assert.assertEquals(130, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_withPremimumPlan_shouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 2;
+        double fare = invoiceServiceForPremimum.calculateFare(distance, time);
+        Assert.assertEquals(15.0, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_withPremimumPlan_shouldReturnInvoiceSummary() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1),
+                new Ride(4.1, 25)
+        };
+        double totalFare = invoiceServiceForPremimum.calculateFareForMultipleRides(rides);
+        Assert.assertEquals(197, totalFare, 0);
+    }
+
+    @Test
+    public void givenUserIdAndRide_withPremimumPlan_shouldReturnInvoiceSummary() {
+        String userId = "gdev3123@gmail.com";
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1),
+                new Ride(4.1, 25)
+        };
+        invoiceServiceForPremimum.addRides(userId);
+        InvoiceSummary invoiceSummary = invoiceServiceForPremimum.getInvoiceSummary(rides);
+        InvoiceSummary fare = new InvoiceSummary(3, 197);
+        Assert.assertEquals(fare, invoiceSummary);
+        Assert.assertEquals(rides.length, invoiceServiceForPremimum.getRidesByUserId(userId).size());
     }
 }
